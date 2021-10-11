c33605469b0fc54dcfa77f2f79be9ef8efdf27fd
Divyanarayana0511 <75729645+Divyanarayana0511@users.noreply.github.com>
2021-10-11 12:22:01 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -3,24 +3,30 @@ function preload(){
 }
 function setup()
 {
-    canvas=createCanvas(400,400);
-    canvas.position(110,250);
+    canvas=createCanvas(1000,1000);
+    canvas.position(100,100);
+    video = createCapture(VIDEO);
+    video.hide();
+
 }
 function draw(){
     background("purple");
-    
+    image(video, 200,200,400,400);
    
    stroke("white");
    strokeWeight(7);
-   line(120,120,250,120);
-   line(250,120,250,250);
-   line(250,250,120,250);
-   line(120,250,120,120);
+   line(120,120,650,120);
+   line(650,120,650,650);
+   line(650,650,120,650);
+   line(120,650,120,120);
    fill("red");
     stroke("green");
     strokeWeight(3);
    circle(120,120,30);
-   circle(250,120,30);
-   circle(250,250,30);
-   circle(120,250,30);
+   circle(650,120,30);
+   circle(650,650,30);
+   circle(120,650,30);
+}
+function takesnapshot(){
+ save("myselfie.png");   
 }
